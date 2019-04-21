5716d88803c83d3414ddbdde3e4df1b87a5928c1
extraterestra <sergiy.kucheryavyy@gmail.com>
2019-04-21 17:40:49 +0200

    commit1

---
+++ src/main/java/itAcademyKtakow/maven/Main.java
@@ -0,0 +1,11 @@
+package itAcademyKtakow.maven;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List l = Lists.newArrayList();
+    }
+}
